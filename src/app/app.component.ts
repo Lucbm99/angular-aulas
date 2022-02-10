8f4473fac47057ce96cdf286ea327c2df217d0e1
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-02-09 22:39:29 -0300

    Ciclos de vida do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
+    <app-title *ngIf="destruir"></app-title>
+    <br/>
+
+    <button (click)="destruirComponent()">Destruir componente</button>
+
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'meu-primeiro-app';
+export class AppComponent implements OnInit, DoCheck, AfterViewInit, AfterContentInit, AfterContentChecked, AfterViewChecked {
+  
+  public valor: number = 1;
+
+  constructor() {
+  }
+
+  public adicionar(): number {
+    return this.valor += 1;
+  }
+
+  public destruir: boolean = true;
+
+  public destruirComponent() {
+    this.destruir = false; 
+
+  }
+
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   console.log(1);
+    // }, 5000)
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit")
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked")
+  }
 }
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnChanges, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() public title: string = "Bem vindo";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    console.log("Foi alterado com sucesso");
+  }
+
+  ngOnDestroy(): void {
+      console.log("Deu bom, ele foi destruído");
+  }
+
+}
