b161c3087d91cda8fe8d97f0f3394f930940d188
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-02-13 15:48:52 -0300

    ngFor e ngIf

---
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-estruturais',
+  templateUrl: './diretivas-estruturais.component.html',
+  styleUrls: ['./diretivas-estruturais.component.scss']
+})
+export class DiretivasEstruturaisComponent implements OnInit {
+
+  public idade: boolean = true;
+  public idadeClick: boolean = true;
+  
+  public list: Array<{ nome: string, idade: number}> = [
+    { nome: "Lucas Baradel", idade: 22 },
+    { nome: "Felicio Campos", idade: 23 },
+    { nome: "Julio Cesar", idade: 22},
+    { nome: "Cezar Marchiori", idade: 33 },
+    { nome: "Rosana Baradel", idade: 60 },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    setInterval( () => {
+      this.idade ? 
+        this.idade = false : 
+        this.idade = true;
+    }, 2000)
+  }
+
+  public onClick() {
+    this.idadeClick ? 
+        this.idadeClick = false : 
+        this.idadeClick = true;
+  }
+
+  public onClickAddList() {
+    this.list.push({ nome: "Fernanda", idade: 31 })
+  }
+
+  public onClickEventList(event: number) {
+    //remover um evento do array
+    this.list.splice(event, 1);
+  }
+}
