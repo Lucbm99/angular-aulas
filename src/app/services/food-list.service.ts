aa6686edb6b7ddb35eae205c1b5e86c49ee8ef75
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-03-02 20:59:19 -0300

    Fix - http get com observable

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { ThrowStmt } from '@angular/compiler';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FoodList } from '../module/food-list';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +17,22 @@ export class FoodListService {
     "Ovo"
   ];
 
-  constructor() { }
+  private url: string = "http://localhost:3000/"; //list-food
 
-  public foodList() {
-    return this.list;
+  constructor(private _httpClient: HttpClient) { 
+
+  }
+
+  // public foodList() {
+  //   return this.list;
+  // }
+
+  public foodList(): Observable<FoodList> {
+    return this._httpClient.get<FoodList>(`${this.url}list-food`)
+    .pipe(
+      res => res,
+      error => error
+    );
   }
 
   public foodListAdd(value: string) {
