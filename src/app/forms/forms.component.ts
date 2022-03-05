e99cd717dcfe8d17c0480a34dc053aa9eeedce91
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-03-05 14:34:04 -0300

    Formulários

---
+++ src/app/forms/forms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit {
+
+  public listFoods: Array<{ comida: string, preco: string }> = [
+    { comida: "X-salada", preco: "R$10,00"},
+    { comida: "X-bacon", preco: "R$11,00"},
+    { comida: "Coxinha", preco: "R$12,00"},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public submitForm(form: NgForm) {
+    console.log(form.value);
+  }
+
+}
